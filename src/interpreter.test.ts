ccca00dd6c9b708dfcdda43a4931f0e0988c0fd5
Logan Zartman <logan.zartman@utexas.edu>
2023-12-22 20:11:59 -0600

    minimal tests

--- src/interpreter.test.ts
+++ src/interpreter.test.ts
@@ -1,8 +1,17 @@
+import dedent from 'dedent';
 import {execBefunge} from './interpreter';
 
 describe('execBefunge', () => {
   it('executes a minimal program', () => {
     const result = execBefunge('1.@');
     expect(result).toBe('1 ');
+  });
+
+  it('can get and set program code', () => {
+    const result = execBefunge(dedent`
+      10g,12g,11g12p12g,@
+      abcdef
+    `);
+    expect(result).toBe('aba');
   });
 });
